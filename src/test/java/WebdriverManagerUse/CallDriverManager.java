69164324349c3bc32655b32a4f040f613a91e52c
varsha seshashayee <varsha.hindupur@gmail.com>
2020-08-20 11:27:26 +0530

    first commit

--- src/test/java/WebdriverManagerUse/CallDriverManager.java
+++ src/test/java/WebdriverManagerUse/CallDriverManager.java
@@ -2,13 +2,16 @@ package WebdriverManagerUse;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class CallDriverManager
 {
 
-	public static void main(String[] args) {
+	@Test
+	public void test1()
+	{
 		// TODO Auto-generated method stub
 
 		WebDriverManager.chromedriver().setup();
@@ -16,7 +19,7 @@ public class CallDriverManager
 //System.setProperty("webdriver.chrome.driver","C:\\Users\\vrshr\\workspace\\SeleniumProject\\src\\test\\resources\\chromedriver.exe");
 		
 		WebDriver browser = new ChromeDriver();
-		
+		System.out.println("Execcuted this class");
 		browser.quit();
 		
 		//firefox, opera, chromium, internet explorer
